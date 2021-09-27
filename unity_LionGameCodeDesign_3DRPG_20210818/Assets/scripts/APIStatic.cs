5a57f4e24b74fbdc1e1b8a92fc18621f91e9036a
liaojyunsian <89112601+liaojyunsian@users.noreply.github.com>
2021-09-27 10:20:56 +0800

    API 練習

---
+++ unity_LionGameCodeDesign_3DRPG_20210818/Assets/scripts/APIStatic.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 認識API靜態 Static
+/// </summary>
+public class APIStatic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        #region 靜態屬性
+        // 取得 Get
+        // 語法：
+        // 類別名稱.靜態屬性
+        float r = Random.value;
+        print("取得靜態屬性．隨機值：" + r);
+
+        // 設定 Set
+        // 語法：
+        // 類別名稱.靜態屬性 指定 值;
+        Cursor.visible = false;
+        #endregion
+
+        #region 靜態方法
+        // 呼叫．參數、傳回
+        // 簽章：參數、傳回
+        // 語法：
+        // 類別名稱.靜態方法(對應引數)
+        float range = Random.Range(10.5f, 20.9f);
+        print("隨機範圍 10.5 ~ 20.9：" + range);
+
+        // ※ API 說明很重要：使用整數時不包含最大值
+        int rangeInt = Random.Range(1, 2);
+        print("隨機範圍 1 ~ 2：" + rangeInt);
+        #endregion
+    }
+
+    private void Update()
+    {
+        #region 靜態屬性
+        // 取得 Get
+        // 語法：
+        // 類別名稱.靜態屬性
+        print("經過多久：" + Time.timeSinceLevelLoad);// time 時間
+        #endregion
+
+    }
+}
