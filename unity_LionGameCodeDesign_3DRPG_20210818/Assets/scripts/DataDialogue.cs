06a6d9ba55d6cdd0813e6bc89ac4797e7c257d20
liaojyunsian <89112601+liaojyunsian@users.noreply.github.com>
2021-10-21 12:01:07 +0800

    新增NPC內容

---
+++ unity_LionGameCodeDesign_3DRPG_20210818/Assets/scripts/DataDialogue.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Sky.Dialogue
+{
+    /// <summary>
+    /// 對話系統的資料
+    /// NPC 要對話的三個階段內容
+    /// 接任務前、任務進行中、完成任務
+    /// </summary>
+    // ScriptableObject 繼承此類別會變成腳本化物件
+    // 可將此腳本資料當成物件保存在專案 Project 內
+    // CreateAssetMenu 類別屬性:為此類別建立專案內選擇
+    // menuName 選單名稱．可用 / 分層
+    // fileName 檔案名稱
+    [CreateAssetMenu(menuName = "Sky/對話資料", fileName = "NPC 對話資料")]
+    public class DataDialogue : ScriptableObject
+    {
+        // 陣列:保存相同資料類型的結構
+        [Header("任務前對話內容"), TextArea(2, 7)]
+        public string[] beforeMission;
+        [Header("任務進行中對話內容"), TextArea(2, 7)]
+        public string[] missionning;
+        [Header("任務完成對話內容"), TextArea(2, 7)]
+        public string[] afterMission;
+        [Header("任務需求數量"), Range(0, 100)]
+        public int countNeed;
+        //
+        //
+        [Header("NPC 任務狀態")]
+        public StaeNPCMission staeNPCMission = StaeNPCMission.BeforeMission;
+    }
+}
---
+++ unity_LionGameCodeDesign_3DRPG_20210818/Assets/scripts/StateNPCMission.cs
@@ -0,0 +1,10 @@
+namespace Sky.Dialogue
+{
+    // 列舉：enum (enumeration)下拉式選單．可自行定義選項
+    // 語法：修飾詞 列舉 列舉名稱 {列舉內容1,...,列舉內容n}
+    // 定義列舉：
+    public enum StaeNPCMission
+    {
+        BeforeMission, Missionning, AfterMission
+    }
+}
